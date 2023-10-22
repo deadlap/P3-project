c6bd90a989b4f56641bd744cb61aa49b258b53cc
deadlap <llabildgaard@hotmail.com>
2023-10-23 00:19:18 +0200

    idk stuff

---
+++ Assets/Resources/Scripts/Test_On_Found.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_On_Found : MonoBehaviour {
+    public void PrintOnFound(string text) {
+        Debug.Log(text);
+    }
+    public void PrintOnExit(string text) {
+        Debug.Log(text);
+    }
+}
