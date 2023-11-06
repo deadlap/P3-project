b369df7e8461cf0e5b36bbd869ba58963f033e53
deadlap <32266596+deadlap@users.noreply.github.com>
2023-11-06 13:43:19 +0100

    Funny slider virker halvt

--- Assets/Scripts/Manual/InstructionAnimationManipulator.cs
+++ Assets/Scripts/Manual/InstructionAnimationManipulator.cs
@@ -2,48 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class InstructionAnimationManipulator : MonoBehaviour
-{
+public class InstructionAnimationManipulator : MonoBehaviour {
     [SerializeField] Slider animProgressSlider;
     Animator currentAnim;
     bool isPlaying;
     float animTime;
     
-    void Start()
-    {
+    void Start() {
         isPlaying = true;
     }
 
-    void Update()
-    {
+    void Update() {
         if(transform.childCount == 0) return;
         if(!isPlaying) return;
         currentAnim = GetComponentInChildren<Animator>();
-        if (animProgressSlider.value < 1)
-        {
-            animProgressSlider.value = animTime;
-            animTime = currentAnim.GetCurrentAnimatorStateInfo(0).normalizedTime;
-        }
-        
-        if (animTime >= 1)
-        {
-            animTime -= currentAnim.GetCurrentAnimatorStateInfo(0).normalizedTime;
-            animProgressSlider.value = 0;
-        }
-        print(animTime);
+        if (!currentAnim) return;
+        animTime = currentAnim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1;
+        animProgressSlider.value = animTime;
     }
     
-    public void ProgressSlider()
-    {
-        currentAnim.SetFloat("AnimationTime", animProgressSlider.value);    
+    public void ProgressSlider() {
+        currentAnim.SetFloat("AnimationTime", animProgressSlider.value);
     }
 
-    public void StartPlayback()
-    {
+    public void StartPlayback() {
         isPlaying = !isPlaying;
+        if (!currentAnim) return;
+        currentAnim.SetFloat("SpeedMultiplier", Convert.ToSingle(isPlaying)); 
+        //Lav en ny ting der hedder isplaying eller noget i stedet for SpeedMultiplier
+        //Da vi skal bruge SpeedMultiplier i ChangeAnimationSpeed
     }
-    void ChangeAnimationSpeed()
-    {
+    
+    void ChangeAnimationSpeed() {
     }
 }
