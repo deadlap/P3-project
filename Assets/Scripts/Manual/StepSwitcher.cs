b90cbee8019bfabe65690ee0e376e445b18f713c
Akaidus <nikolajjensen6854@gmail.com>
2023-11-21 13:03:23 +0100

    slider og speed menu
    - speed menu lukker når man skifter trin
    - slider forsvinder på "du har vundet"-trinet

--- Assets/Scripts/Manual/StepSwitcher.cs
+++ Assets/Scripts/Manual/StepSwitcher.cs
@@ -8,11 +8,12 @@ public class StepSwitcher : MonoBehaviour {
     [SerializeField] GameObject finishManualButton;
     [SerializeField] GameObject nextStepButton;
     [SerializeField] GameObject previousStepButton;
+    [SerializeField] GameObject sliderArea;
 
-    void Start()
-    {
+    void Start() {
         nextStepButton.SetActive(true);
         previousStepButton.SetActive(false);
+        sliderArea.SetActive(true);
     }
 
     public void ChangeStep(int change){
@@ -26,14 +27,17 @@ public class StepSwitcher : MonoBehaviour {
             InstructionAnimationManipulator.instance.OpenSpeedMenu();
         if (StepCounter.CurrentStep == 1) {
             previousStepButton.SetActive(false);
+            sliderArea.SetActive(true);
         } else if (StepCounter.CurrentStep < StepCounter.MaxSteps && StepCounter.CurrentStep != 1) {
             previousStepButton.SetActive(true);
             nextStepButton.SetActive(true);
             finishManualButton.SetActive(false);
+            sliderArea.SetActive(true);
         } else {
             previousStepButton.SetActive(true);
             nextStepButton.SetActive(false);
             finishManualButton.SetActive(true);
+            sliderArea.SetActive(false);
         }
     }
 }
