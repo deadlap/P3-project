25d9ce6664f4da5f99ce7c3c085c5af41d4e4ff0
deadlap <32266596+deadlap@users.noreply.github.com>
2023-12-01 11:29:37 +0100

    nyt stuff

--- Assets/Scripts/PrecisionTesting/WriteObjectName.cs
+++ Assets/Scripts/PrecisionTesting/WriteObjectName.cs
@@ -7,13 +7,16 @@ public class WriteObjectName : MonoBehaviour {
     [SerializeField] private TMP_Text textBox;
     [SerializeField] private string standardText;
     [SerializeField] private string foundObjectText;
+    
+    void Start(){
+        textBox.text = "Start";
+    }
 
     public void OnObjectFound(GameObject objectFound){
         textBox.text = foundObjectText + objectFound.name;
     }
 
-
     public void OnObjectLost(GameObject objectLost){
-        textBox.text = standardText;
+        textBox.text = standardText + objectLost.name;
     }
 }
