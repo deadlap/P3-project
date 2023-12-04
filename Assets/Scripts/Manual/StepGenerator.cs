7825ff9df9c0d645abc8ecad024c5aca26e72ab1
deadlap <32266596+deadlap@users.noreply.github.com>
2023-12-04 12:33:04 +0100

    shtuff

--- Assets/Scripts/Manual/StepGenerator.cs
+++ Assets/Scripts/Manual/StepGenerator.cs
@@ -16,7 +16,7 @@ public class StepGenerator : MonoBehaviour {
         if (ManualFetcher.CurrentParts().Count <= 0) return;
         for (int i = 0; i < ManualFetcher.CurrentParts().Count; i++) {
             GameObject button = GameObject.Instantiate(buttonPrefab, this.transform);
-            button.GetComponentInChildren<PartButton>().SetPart(
+            button.GetComponent<PartButton>().SetPart(
                 thisStep.parts[i],
                 thisStep.amounts[i],
                 thisStep.parts[i].GetComponent<PartInfo>().partSprite
